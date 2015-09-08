36bbe41252840cb35af3350c1db7cede26a6ad22
Valishin Marat <valmar.ktn@gmail.com>
2015-09-08 22:33:13 +0300

    Zz

---
+++ AngularJS_Tests/WebContent/modules/home/controllers.js
@@ -0,0 +1,70 @@
+﻿'use strict';
+
+angular.module('Home')
+
+.factory('List', function ($resource) {
+	  return $resource("/AngularJS_Tests/task/:username.json");
+	}
+)
+
+.factory('Entry', function($resource){
+	var Resource = $resource("/AngularJS_Tests/task/:user.json");
+	return {	
+		getById: function(username, id, callback){
+			Resource.query({user: username}, function(data){
+				for (var i=0;i<data.length;i++){
+					if (data[i].id == id) {
+						callback(data[i]);
+						return;
+					}
+				};	
+			});
+		},
+		getList: function(username, callback){
+			Resource.query({user:username}, callback);
+		}
+	};
+})
+
+
+.controller('DetailController', ['$scope', '$routeParams', '$rootScope', 'Entry', '$timeout', function($scope, $routeParams, $rootScope, Entry, $timeout){
+	$scope.username = $rootScope.globals.currentUser.username;
+	
+	(function update() {
+	    $timeout(update, 10000);
+			Entry.getById($scope.username, $routeParams.id, function(item){
+				$scope.item = item;
+			});
+	  }());
+}])
+
+.controller('HomeController', ['$scope', '$http', '$rootScope', 'Entry', '$location','$timeout',
+    function ($scope, $http, $rootScope, Entry, $location, $timeout) {
+		$scope.username = $rootScope.globals.currentUser.username;
+		
+		(function update() {
+		    $timeout(update, 10000);
+					Entry.getList($scope.username, function(data){
+						$scope.tasks = data;
+					});
+		  }());
+
+		$scope.filterItems = {
+			    'В процессе': true,
+			    'Выполнено': true,
+			    'Отменено': true
+			  };
+		$scope.items = [
+			    {name:'В процессе'}, 
+			    {name:'Выполнено'}, 
+			    {name:'Отменено'}
+			  ];
+		//filter
+		  $scope.testFilter = function (x) {
+		    return $scope.filterItems[x.status];
+		  };
+		  
+		 $scope.goto_detail=function(id){
+			 $location.url('/detail/' + id);
+		 };
+    }]);
---
+++ AngularJS_Tests/WebContent/scripts/app.js
@@ -0,0 +1,51 @@
+﻿'use strict';
+
+// declare modules
+angular.module('Authentication', []);
+angular.module('Home', ['ngResource']);
+
+angular.module('SimbirSoftApp', [
+    'Authentication',
+    'Home',
+    'ngRoute',
+    'ngCookies'
+])
+
+
+
+.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'modules/authentication/views/login.html'
+        })
+
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'modules/home/views/home.html'
+        })
+
+        .when('/detail/:id', {
+            controller: 'DetailController',
+            templateUrl: 'modules/home/views/detail.html'
+        })
+        
+        
+        .otherwise({ redirectTo: '/login' });
+}])
+
+.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }]);
